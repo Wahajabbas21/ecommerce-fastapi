350dd433edf726eb31589a5f11152b9d6cedbeee
Wahajabbas21 <wahajabbas890@gmail.com>
2026-09-14 22:44:30 +0500

    Update index.py with python path fix

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,10 @@
+import sys
+import os
+
+# Ensure the current directory is in python path
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app.main import app as fastapi_app
 from mangum import Mangum
 
 app = Mangum(fastapi_app)
-
-
